eb4c0c57d950bf5a4d70d07a8128543791c594b8
Kevin Liao <lwk723@berkeley.edu>
2016-11-05 14:21:19 -0700

    Modified Makefile: change directory

--- Makefile
+++ Makefile
@@ -7,17 +7,24 @@ data:
 	curl -O http://www-bcf.usc.edu/~gareth/ISL/Advertising.csv > data
 
 report/report.pdf: report/report.Rmd data/regression.RData images/scatterplot-tv-sales.png
-	pandoc -s report/report.Rmd -o report/report.pdf
+	cd report && Rscript -e “library(rmarkdown); render(‘report.Rmd’)”
+	#cd report && pandoc report.Rmd -o report.pdf
+	#pandoc -s report/report.Rmd -o report/report.pdf
 
 data/regression.RData: code/regression-script.R data/Advertising.csv
-	Rscript code/regression-script.R
+	cd code && Rscript regression-script.R
+	
 
 data/eda-output.txt: code/eda-script.R data/Advertising.csv
-	Rscript code/eda-script.R
+	cd code && Rscript eda-script.R
+	
 
 
 clean:
 	rm -f report/report.pdf report/report.Rmd
+
+
+# use xtable in Rscript file R chunk to display regression table
 
 
 
--- code/regression-script.R
+++ code/regression-script.R
@@ -1,19 +1,19 @@
 
 # Read Advertising data into R
-Advertising <- read.csv(file = "data/Advertising.csv", header = TRUE)
+Advertising <- read.csv(file = "../data/Advertising.csv", header = TRUE)
 
 # Run Simple Linear Regression of Sales and TV
 lm <- lm(Sales ~ TV, data = Advertising)
 
 # Output summary of the regression to regression.RData
 regression <- summary(lm)
-save(regression, file = 'data/regression.RData')
+save(regression, file = '../data/regression.RData')
 
 # Save scatter plot of regression in both PNG and PDF
 plot(x = Advertising$TV, y = Advertising$Sales, main = "Regression Analysis of Sales and TV",
      xlab = "TV", ylab = "Sales", col = "red", cex = 0.5)
 abline(lm, col = "blue")
-dev.copy(png, 'images/scatterplot-tv-sales.png')
+dev.copy(png, '../images/scatterplot-tv-sales.png')
 dev.off()
-dev.copy(pdf, 'images/scatterplot-tv-sales.pdf')
+dev.copy(pdf, '../images/scatterplot-tv-sales.pdf')
 dev.off()
